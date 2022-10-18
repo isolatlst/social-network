765f48d24afbbfb7972b78747c1938397d6bcb43
isolatlst <kirill.gurin20152016@yandex.ru>
2022-10-18 06:00:21 +0300

    a little of Hook useEffect

--- src/components/Profile/Description/DescriptionText.jsx
+++ src/components/Profile/Description/DescriptionText.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import classes from './Description.module.css'
-import loading from '../../../assets/icons/loading.gif'
 import { useState } from 'react'
+import { useEffect } from 'react'
 
 
 
@@ -9,27 +9,32 @@ const DescriptionText = (props) => {
 	const [editMode, toggleEditMode] = useState(false)
 	const [data, setData] = useState(props.data)
 	const [isFetching, toggleIsFetching] = useState(false)
+	useEffect(() => {
+		toggleIsFetching(false)
+		setData(props.data)
+	}, [props.data])
 
 	const onChangeData = (e) => setData(e.currentTarget.value)
 	const activateEditMode = () => toggleEditMode(true)
-	const deactivateEditMode = async () => {
+	const deactivateEditMode = () => {
 		toggleEditMode(false)
 		toggleIsFetching(true)
-		await props.updateProfile(data, props.type)
-		toggleIsFetching(false)
+		props.updateProfile(data, props.type)
 	}
 
 	return (
 		editMode
 			? < div className={`${classes.description__text} ${classes.description__text_edit_mode}`} >
-				{props.text}&nbsp;
+				{props.text}
+				&nbsp;
 				<input onChange={onChangeData} onBlur={deactivateEditMode} autoFocus type="text" value={data} />
 			</div>
 			: < div className={classes.description__text} >
-				{isFetching ? <img className={classes.isFetchingImage} src={loading} alt='...loading' /> : ''}
-				{props.text}&nbsp;
+				{isFetching ? <span className={classes.isFetchingBlock} /> : ''}
+				{props.text}
+				&nbsp;
 				<span onDoubleClick={props.isMineProfile ? activateEditMode : null}>
-					{props.data}
+					{data}
 				</span>
 			</ div>
 	)
